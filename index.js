e9cf184bced7862c6e4740b340b7a085a2cd1388
keval718 <kevalvirat@gmail.com>
2020-05-15 08:19:43 -0400

    route created for user in index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const userRoute=require('./routes/UserR')
 app.use(cors());
 
 app.use(express.json());
 const connectDB = require('./config/connectDB');
 const port = process.env.PORT || 5000;
 connectDB();
+app.user('/api/users',userRoute);
+
+
 app.listen(port, () => console.log(`server running on port ${port}`));
