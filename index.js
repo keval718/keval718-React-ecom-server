9fa61b46223592370756f322e13cbc6b56197af2
keval718 <kevalvirat@gmail.com>
2020-05-15 12:24:18 -0400

    product routes added

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const userRoute=require('./routes/UserR')
+const userRoute=require('./routes/UserR');
+const productRoute=require('./routes/ProductR');
 app.use(cors());
 app.use(express.json());
 const connectDB = require('./config/connectDB');
 const port = process.env.PORT || 5000;
 connectDB();
 app.use('/api/users',userRoute);
+app.use('/api/products',productRoute);
 
 
 
